fc9aca709045be4156e1f47b1490d7e28a1e44ed
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2023-03-01 01:59:10 +0530

    Photo Search Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Mountain from "./images/mountain.jpg";
+
+import SearchPhotos from "./SearchPhotos";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <img src={Mountain} className="header-img" />
+        <h1 className='title'>Download High Quality Images by creators</h1>
+        <SearchPhotos />
+      </div>
     </div>
   );
 }
---
+++ src/SearchPhotos.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import Unsplash, { toJson } from "unsplash-js";
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Modal from '@mui/material/Modal';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Like from "./images/like.jpg";
+import { Icon } from "@mui/material";
+import { display } from "@mui/system";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    // width: 600,
+    // bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    maxHeight: '85%'
+};
+
+const unsplash = new Unsplash({
+    accessKey: "q7lozWCfZdigPHRnuIrMGyIDzoZawouOg0sD0cySEEc",
+});
+export default function SearchPhotos() {
+
+    const [query, setQuery] = useState("");
+    const [pics, setPics] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [modalimg, setModalImg] = useState("");
+    const [modalchipimg, setModalChipimg] = useState("");
+    const [modalname, setModalName] = useState("");
+    const [modallikes, setModalLikes] = useState("");
+    const [modalinsta, setModalInsta] = useState("");
+    const [tags, setTags] = useState([]);
+
+
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+
+        unsplash.search
+            .photos(query, 1, 25)
+            .then(toJson)
+            .then((json) => {
+                console.log(json);
+                setPics(json.results);
+            });
+
+    };
+
+    function handleOpen(val) {
+        setModalImg(val.urls.full);
+        setModalChipimg(val.user.profile_image.medium);
+        setModalName(val.user.name);
+        setModalLikes(val.likes);
+        setModalInsta(val.user.instagram_username);
+        console.log("val tags are ", val.tags);
+        setTags(val.tags);
+        setOpen(true);
+
+    }
+    const handleClose = () => setOpen(false);
+
+    return (
+        <>
+            <form className="form" onSubmit={searchPhotos}>
+                {" "}
+                <label className="label" htmlFor="query">
+                    {" "}
+                    📷
+                </label>
+                <input
+                    type="text"
+                    name="query"
+                    className="input"
+                    placeholder={`Try "cat" or "apple"`}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit" className="button">
+                    Search
+                </button>
+            </form>
+
+            <div className="card-list">
+                {pics.map((pic) => (
+                    <div>
+                        <div className="card" key={pic.id} onClick={() => handleOpen(pic)}>
+                            <img
+                                className="card--image"
+                                alt={pic.alt_description}
+                                src={pic.urls.full}
+                                width="50%"
+                                height="50%"
+                            ></img>
+                        </div>
+                        <div className="card-info">
+                            <div className="chip">
+                                <img src={pic.user.profile_image.medium} alt="Person" width="96" height="96"></img>
+                                {pic.user.name}
+                            </div>
+                            <div className="chip">
+                                <img src={Like} alt="Like" style={{ width: "36px", height: "36px", marginTop: "0.1rem" }}></img>
+                                {pic.likes}
+                            </div>
+                        </div>
+                    </div>
+                ))}{" "}
+            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <img
+                        className="modal-image"
+                        alt="Modal Image"
+                        src={modalimg}
+                        height='400'
+                        width="100%"
+                    ></img>
+                    <div className="card-info">
+                        <div className="chip">
+                            <img src={modalchipimg} alt="Person" width="96" height="96"></img>
+                            {modalname}
+                        </div>
+                        <div className="chip">
+                            <img src={Like} alt="Like" style={{ width: "36px", height: "36px", marginTop: "0.1rem" }}></img>
+                            {modallikes}
+                        </div>
+                        <div className="chip">
+                            <InstagramIcon fontSize="large" />
+                            {modalinsta}
+                        </div>
+                    </div>
+                    <h2 className="heading2">Related Tags</h2>
+
+                    <div className="modal-outside-tag">
+                        {
+                            tags.map((tag) => (
+                                // console.log("tag is ", tag.title)
+                                < div className="chip" >
+                                    {tag.title}
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                </Box>
+            </Modal>
+        </>
+    );
+}
